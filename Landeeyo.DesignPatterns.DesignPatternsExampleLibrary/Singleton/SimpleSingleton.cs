05ff150e7dc089aab7a35b5949dab26317944e3b
landeeyo <landeeyo@10g.pl>
2014-04-21 15:58:26 +0200

    Added singleton pattern.

---
+++ Landeeyo.DesignPatterns.DesignPatternsExampleLibrary/Singleton/SimpleSingleton.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Landeeyo.DesignPatterns.DesignPatternsExampleLibrary.Singleton
+{
+    public sealed class SimpleSingleton
+    {
+        private static readonly SimpleSingleton instance = new SimpleSingleton();
+
+        private SimpleSingleton() { }
+
+        public static SimpleSingleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public static string Execute()
+        {
+            return "It's me, singleton!";
+        }
+    }
+}
---
+++ Landeeyo.DesignPatterns.Tests/Singleton/SingletonTest.cs
@@ -0,0 +1,22 @@
+﻿using Landeeyo.DesignPatterns.DesignPatternsExampleLibrary.Singleton;
+using NUnit.Framework;
+
+namespace Landeeyo.DesignPatterns.Tests.Singleton
+{
+    [TestFixture]
+    public class SingletonTest
+    {
+        [Test]
+        public void SimpleSingletonTest()
+        {
+            //arrange
+            var properResult = "It's me, singleton!";
+
+            //act
+            string result = SimpleSingleton.Execute();
+
+            //assert
+            Assert.AreEqual(properResult, result);
+        }
+    }
+}
