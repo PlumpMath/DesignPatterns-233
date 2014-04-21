6d4bcf8943a07c905232bd2aeade0449f1b21589
landeeyo <landeeyo@10g.pl>
2014-04-21 15:34:00 +0200

    Implemented nUnit tests for the rest of concrete strategies. A bit of cleanup has been done.

--- Landeeyo.DesignPatterns.Tests/Strategy/StrategyTest.cs
+++ Landeeyo.DesignPatterns.Tests/Strategy/StrategyTest.cs
@@ -27,5 +27,39 @@ namespace Landeeyo.DesignPatterns.Tests.Strategy
             Assert.AreEqual(properResult, result);
         }
 
+        [Test]
+        public void ConcreteStrategySubtractTest()
+        {
+            //arrange
+            var a = 2;
+            var b = 2;
+            var properResult = 0;
+            IStrategy strategy = new Add();
+            StrategyContext context = new StrategyContext(strategy);
+
+            //act
+            var result = context.Execute(a, b);
+
+            //assert
+            Assert.AreEqual(properResult, result);
+        }
+
+        [Test]
+        public void ConcreteStrategyMultiplyTest()
+        {
+            //arrange
+            var a = 2;
+            var b = 2;
+            var properResult = 4;
+            IStrategy strategy = new Add();
+            StrategyContext context = new StrategyContext(strategy);
+
+            //act
+            var result = context.Execute(a, b);
+
+            //assert
+            Assert.AreEqual(properResult, result);
+        }
+
     }
 }
