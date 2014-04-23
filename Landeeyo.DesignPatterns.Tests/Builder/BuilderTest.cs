5aeab4be3ad6e912e8f89df257f3092753b0b624
Landeeyo <landeeyo@10g.pl>
2014-04-23 21:44:00 +0200

    Added bulid pattern test.

---
+++ Landeeyo.DesignPatterns.Tests/Builder/BuilderTest.cs
@@ -0,0 +1,28 @@
+﻿using Landeeyo.DesignPatterns.DesignPatternsExampleLibrary.Builder;
+using NUnit.Framework;
+
+namespace Landeeyo.DesignPatterns.Tests.Builder
+{
+    [TestFixture]
+    public class BuilderTest
+    {
+        [Test]
+        public void ConnectionBuilderTest()
+        {
+            //arrange
+            Connection expectedResult =
+                new Connection()
+                {
+                    Address = "google.com",
+                    Port = 80,
+                    Type = ConnectionType.TCP
+                };
+            Connection result = null;
+
+            //act
+
+            //assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
